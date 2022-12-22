0a1fb76ae5f0176117c45a83652f71d8724b7013
Mateusz-Roman <mateusz.maciej.roman@gmail.com>
2022-12-22 19:43:23 +0100

    NavBar

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Center(
         child: Text("Home"),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.masks_outlined), label: "Powietrze"),
-          BottomNavigationBarItem(icon: Icon(Icons.wb_cloudy_outlined), label: "Pogoda")
+          BottomNavigationBarItem(
+              icon: Icon(Icons.masks_outlined), label: "Powietrze"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.wb_cloudy_outlined), label: "Pogoda")
         ],
       ),
     );
---
+++ lib/WeatherScreen.dart
@@ -0,0 +1,102 @@
+import 'package:clean_air/PermissionScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'MyHomePage.dart';
+import 'main.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+        const Duration(seconds: 2),
+        () => {
+              if (havePermission())
+                {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => PermissionScreen()))
+                }
+              else
+                {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()))
+                }
+            });
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: new Color(0xffffffff),
+              gradient: LinearGradient(
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+                colors: [new Color(0xff6671e5), new Color(0xff4852d9)],
+              )
+            ),
+
+          ),
+          Align(
+            alignment: FractionalOffset.center,
+            child:
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  Image(
+                    image: AssetImage('icons/cloud-sun.png')
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 15.0)),
+                  Text(Strings.appTitle,
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                      fontSize: 42.0,
+                      color: Colors.white,
+                    ))),
+                Padding(padding: EdgeInsets.only(top: 5.0)),
+                  Text('Monitorowanie czystości powietrza',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                    textStyle: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    )
+                  )),
+                ],
+             )
+          ),
+          Positioned(
+            bottom: 35,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              child: Text("Przywiewam dane...",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.w300,
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ))),
+            ))
+        ]
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  bool havePermission() {
+    return true;
+  }
+}
